de08938cc80d5a1ebc5111102e6a7155e1b7b8ae
Ahmed Matoussi <ahmedgeek1@gmail.com>
2017-11-22 15:25:42 -0600

    Add files via Upload
    Add all files and the project's directory of the project.

---
+++ Concatenation App/Assessment 3B/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace Assessment_3B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxMiddleName = new System.Windows.Forms.TextBox();
+            this.txtBoxTitle = new System.Windows.Forms.TextBox();
+            this.txtBoxLastName = new System.Windows.Forms.TextBox();
+            this.txtBoxFirstName = new System.Windows.Forms.TextBox();
+            this.lblFirstNamePrompt = new System.Windows.Forms.Label();
+            this.lblTitlePrompt = new System.Windows.Forms.Label();
+            this.lblLastNamePrompt = new System.Windows.Forms.Label();
+            this.lblOutPut1 = new System.Windows.Forms.Label();
+            this.lblOutPut2 = new System.Windows.Forms.Label();
+            this.lblOutPut3 = new System.Windows.Forms.Label();
+            this.lblOutPut4 = new System.Windows.Forms.Label();
+            this.lblMiddleNamePrompt = new System.Windows.Forms.Label();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBoxMiddleName
+            // 
+            this.txtBoxMiddleName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxMiddleName.Location = new System.Drawing.Point(173, 128);
+            this.txtBoxMiddleName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBoxMiddleName.Name = "txtBoxMiddleName";
+            this.txtBoxMiddleName.Size = new System.Drawing.Size(217, 22);
+            this.txtBoxMiddleName.TabIndex = 0;
+            // 
+            // txtBoxTitle
+            // 
+            this.txtBoxTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxTitle.Location = new System.Drawing.Point(173, 39);
+            this.txtBoxTitle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBoxTitle.Name = "txtBoxTitle";
+            this.txtBoxTitle.Size = new System.Drawing.Size(217, 22);
+            this.txtBoxTitle.TabIndex = 1;
+            // 
+            // txtBoxLastName
+            // 
+            this.txtBoxLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxLastName.Location = new System.Drawing.Point(173, 174);
+            this.txtBoxLastName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBoxLastName.Name = "txtBoxLastName";
+            this.txtBoxLastName.Size = new System.Drawing.Size(217, 22);
+            this.txtBoxLastName.TabIndex = 2;
+            // 
+            // txtBoxFirstName
+            // 
+            this.txtBoxFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxFirstName.Location = new System.Drawing.Point(173, 84);
+            this.txtBoxFirstName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBoxFirstName.Name = "txtBoxFirstName";
+            this.txtBoxFirstName.Size = new System.Drawing.Size(217, 22);
+            this.txtBoxFirstName.TabIndex = 3;
+            // 
+            // lblFirstNamePrompt
+            // 
+            this.lblFirstNamePrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstNamePrompt.Location = new System.Drawing.Point(27, 87);
+            this.lblFirstNamePrompt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFirstNamePrompt.Name = "lblFirstNamePrompt";
+            this.lblFirstNamePrompt.Size = new System.Drawing.Size(133, 28);
+            this.lblFirstNamePrompt.TabIndex = 4;
+            this.lblFirstNamePrompt.Text = "First Name";
+            this.lblFirstNamePrompt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTitlePrompt
+            // 
+            this.lblTitlePrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitlePrompt.Location = new System.Drawing.Point(27, 43);
+            this.lblTitlePrompt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitlePrompt.Name = "lblTitlePrompt";
+            this.lblTitlePrompt.Size = new System.Drawing.Size(133, 28);
+            this.lblTitlePrompt.TabIndex = 5;
+            this.lblTitlePrompt.Text = "Title";
+            this.lblTitlePrompt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLastNamePrompt
+            // 
+            this.lblLastNamePrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastNamePrompt.Location = new System.Drawing.Point(27, 177);
+            this.lblLastNamePrompt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblLastNamePrompt.Name = "lblLastNamePrompt";
+            this.lblLastNamePrompt.Size = new System.Drawing.Size(133, 28);
+            this.lblLastNamePrompt.TabIndex = 6;
+            this.lblLastNamePrompt.Text = "Last Name";
+            this.lblLastNamePrompt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOutPut1
+            // 
+            this.lblOutPut1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutPut1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutPut1.Location = new System.Drawing.Point(599, 36);
+            this.lblOutPut1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOutPut1.Name = "lblOutPut1";
+            this.lblOutPut1.Size = new System.Drawing.Size(331, 28);
+            this.lblOutPut1.TabIndex = 7;
+            this.lblOutPut1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOutPut2
+            // 
+            this.lblOutPut2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutPut2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutPut2.Location = new System.Drawing.Point(599, 80);
+            this.lblOutPut2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOutPut2.Name = "lblOutPut2";
+            this.lblOutPut2.Size = new System.Drawing.Size(331, 28);
+            this.lblOutPut2.TabIndex = 8;
+            this.lblOutPut2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOutPut3
+            // 
+            this.lblOutPut3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutPut3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutPut3.Location = new System.Drawing.Point(599, 124);
+            this.lblOutPut3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOutPut3.Name = "lblOutPut3";
+            this.lblOutPut3.Size = new System.Drawing.Size(331, 28);
+            this.lblOutPut3.TabIndex = 9;
+            this.lblOutPut3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOutPut4
+            // 
+            this.lblOutPut4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutPut4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutPut4.Location = new System.Drawing.Point(599, 170);
+            this.lblOutPut4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOutPut4.Name = "lblOutPut4";
+            this.lblOutPut4.Size = new System.Drawing.Size(331, 28);
+            this.lblOutPut4.TabIndex = 10;
+            this.lblOutPut4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMiddleNamePrompt
+            // 
+            this.lblMiddleNamePrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMiddleNamePrompt.Location = new System.Drawing.Point(27, 132);
+            this.lblMiddleNamePrompt.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMiddleNamePrompt.Name = "lblMiddleNamePrompt";
+            this.lblMiddleNamePrompt.Size = new System.Drawing.Size(133, 28);
+            this.lblMiddleNamePrompt.TabIndex = 11;
+            this.lblMiddleNamePrompt.Text = "Middle Name";
+            this.lblMiddleNamePrompt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.Location = new System.Drawing.Point(292, 245);
+            this.btnDisplay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(100, 28);
+            this.btnDisplay.TabIndex = 12;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(444, 245);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 28);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(599, 245);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 28);
+            this.btnClose.TabIndex = 14;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 335);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.lblMiddleNamePrompt);
+            this.Controls.Add(this.lblOutPut4);
+            this.Controls.Add(this.lblOutPut3);
+            this.Controls.Add(this.lblOutPut2);
+            this.Controls.Add(this.lblOutPut1);
+            this.Controls.Add(this.lblLastNamePrompt);
+            this.Controls.Add(this.lblTitlePrompt);
+            this.Controls.Add(this.lblFirstNamePrompt);
+            this.Controls.Add(this.txtBoxFirstName);
+            this.Controls.Add(this.txtBoxLastName);
+            this.Controls.Add(this.txtBoxTitle);
+            this.Controls.Add(this.txtBoxMiddleName);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxMiddleName;
+        private System.Windows.Forms.TextBox txtBoxTitle;
+        private System.Windows.Forms.TextBox txtBoxLastName;
+        private System.Windows.Forms.TextBox txtBoxFirstName;
+        private System.Windows.Forms.Label lblFirstNamePrompt;
+        private System.Windows.Forms.Label lblTitlePrompt;
+        private System.Windows.Forms.Label lblLastNamePrompt;
+        private System.Windows.Forms.Label lblOutPut1;
+        private System.Windows.Forms.Label lblOutPut2;
+        private System.Windows.Forms.Label lblOutPut3;
+        private System.Windows.Forms.Label lblOutPut4;
+        private System.Windows.Forms.Label lblMiddleNamePrompt;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ Concatenation App/Assessment 3B/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assessment_3B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            try
+            {   //Conditions on the textboxes - Name should be in Letters (no numbers accepted for example)
+                if (!System.Text.RegularExpressions.Regex.IsMatch(txtBoxTitle.Text, "^[a-zA-Z]"))
+                {
+                    MessageBox.Show("You should enter a valid Title (No numbers or empty fields are accepted)");
+                    txtBoxTitle.Text.Remove(txtBoxTitle.Text.Length - 1);
+                }
+                else if (!System.Text.RegularExpressions.Regex.IsMatch(txtBoxLastName.Text, "^[a-zA-Z]"))
+                {
+                    MessageBox.Show("You should enter a valid Last Name (No numbers or empty fields are accepted)");
+                    txtBoxLastName.Text.Remove(txtBoxLastName.Text.Length - 1);
+                }
+                else if (!System.Text.RegularExpressions.Regex.IsMatch(txtBoxFirstName.Text, "^[a-zA-Z]"))
+                {   MessageBox.Show("You should enter a valid First Name (No numbers or empty fields are accepted)");
+                txtBoxFirstName.Text.Remove(txtBoxFirstName.Text.Length - 1);
+                } else if (!System.Text.RegularExpressions.Regex.IsMatch(txtBoxMiddleName.Text, "^[a-zA-Z]"))
+                {   MessageBox.Show("You should enter a valid Middle Name (No numbersor empty fields are accepted)");
+                    txtBoxMiddleName.Text.Remove(txtBoxMiddleName.Text.Length - 1);
+                }
+                else { 
+                //display Title First Middle Last
+                lblOutPut1.Text = txtBoxTitle.Text + " " + txtBoxFirstName.Text + " " + txtBoxMiddleName.Text + " " + txtBoxLastName.Text;
+                //display Last, First Middle
+                lblOutPut2.Text = txtBoxLastName.Text + ", " + txtBoxFirstName.Text + " " + txtBoxMiddleName.Text;
+                //display Title Last
+                lblOutPut3.Text = txtBoxTitle.Text + " " + txtBoxLastName.Text;
+                //display Last, First, Title    
+                lblOutPut4.Text = txtBoxLastName.Text + ", " + txtBoxFirstName.Text + ", " + txtBoxTitle.Text;
+                }
+            }
+            catch(Exception ex) {
+                //display the default error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // Reset all textboxes to blank
+            txtBoxTitle.Text = "";
+            txtBoxFirstName.Text = "";
+            txtBoxMiddleName.Text = "";
+            txtBoxLastName.Text = "";
+
+            //Reset all output labels to blank
+            lblOutPut1.Text = "";
+            lblOutPut2.Text = "";
+            lblOutPut3.Text = "";
+            lblOutPut4.Text = "";
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    } 
+}
